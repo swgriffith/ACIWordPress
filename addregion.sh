fdbcc6cfc2dab30c5e9278db451f33fd748214ee
Steve Griffith <steve.griffith@microsoft.com>
2018-06-28 00:12:07 +0000

    Added readme and fixed script

--- addregion.sh
+++ addregion.sh
@@ -22,5 +22,5 @@ echo Priority: $PRIORITY
 az container create -g $RG --name $NAME-$LOC --location $LOC --image wordpress --ip-address Public --os-type Linux --environment-variables WORDPRESS_DB_HOST=$DBNAME.mysql.database.azure.com WORDPRESS_DB_USER=griffith\@$DBNAME WORDPRESS_DB_PASSWORD=JWJIxoN7qdfd6F
 
 
-az network traffic-manager endpoint create --name $NAME-$LOC --profile-name WordpressTrafficMngr -g $RG --type externalEndpoints --target $NAME.$LOC.azurecontainer.io --priority $PRIORITY
+az network traffic-manager endpoint create --name $NAME-$LOC --profile-name WordpressTrafficMngr -g $RG --type externalEndpoints --target $NAME.$LOC.azurecontainer.io --endpoint-location $LOC
 
